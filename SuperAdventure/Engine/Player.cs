a775b482b87c6194291e4af45a370666264582f0
SnipeHub <v-macar2@microsoft.com>
2018-04-16 19:43:45 +1000

    Part 7.1 - inheritance and base classes. Bascially created a few base classes for other classes to inherit the same properties, pretty cool :-}

--- SuperAdventure/Engine/Player.cs
+++ SuperAdventure/Engine/Player.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace Engine
 {
-    public class Player  // Using public here ensues this class is visible throughout the wole solution
+    public class Player : LivingCreature // Using public here ensues this class is visible throughout the wole solution
     {
-        public int CurrentHitPoints { get; set; }  // Here we create our int named CurrentHitPoints etc
-        public int MaximumHitPoints { get; set; }
-        public int Gold { get; set; }
+        //public int CurrentHitPoints { get; set; }  These are inherited under LivingCreature.cs
+        //public int MaximumHitPoints { get; set; }
+        public int Gold { get; set; } // Here we create our int named Gold etc
         public int ExperiencePoints { get; set; }
         public int Level { get; set; }
     }
