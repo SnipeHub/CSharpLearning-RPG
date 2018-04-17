9c0f01a7663987341d6ac83632ff0c36a70091ef
SnipeHub <v-macar2@microsoft.com>
2018-04-17 20:34:11 +1000

    commented out no longer needed constructor code

--- SuperAdventure/SuperAdventure/SuperAdventure.cs
+++ SuperAdventure/SuperAdventure/SuperAdventure.cs
@@ -19,10 +19,10 @@ namespace SuperAdventure
         {
             InitializeComponent();
 
-            Location location = new Location(1, "Home", "This is your house."); // This is our location object
-            location.ID = 1;
-            location.Name = "Home";
-            location.Description = "This is your house.";
+            //Location location = new Location(1, "Home", "This is your house."); // This is our location object. We no longer need this line as we have created our own in the required classes.
+            //location.ID = 1;
+            //location.Name = "Home";
+            //location.Description = "This is your house.";
 
             _player = new Player();
 
