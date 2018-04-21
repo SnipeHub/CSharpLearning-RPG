02ccb7e579c10a3685e33dd6f9197cc0bf616ea3
SnipeHub <v-macar2@microsoft.com>
2018-04-21 22:33:10 +1000

    Part 10.1 - Creating collections of objects

--- SuperAdventure/Engine/Player.cs
+++ SuperAdventure/Engine/Player.cs
@@ -14,11 +14,17 @@ namespace Engine
         public int ExperiencePoints { get; set; }
         public int Level { get; set; }
 
+        public List<InventoryItem> Inventory { get; set; }
+        public List<PlayerQuest> Quests { get; set; }
+
         public Player(int currentHitPoints, int maximumHitPoints, int gold, int experiencePoints, int level) : base(currentHitPoints, maximumHitPoints)
         {
             Gold = gold;
             ExperiencePoints = experiencePoints;
             Level = level;
+
+            Inventory = new List<InventoryItem>(); // Here we set the value of the new properties to empty lists, which means we can add items to them later.
+            Quests = new List<PlayerQuest>();
         }
     }
 }
